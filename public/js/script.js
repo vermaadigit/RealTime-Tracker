32cfaf75a3d44376e6acccb31cbb0bef6e4bfe62
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-10-04 00:10:09 +0530

    Pubilc File

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+const socket = io()
+
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition((position) => {
+        const {latitude, longitude} = position.coords
+        socket.emit("send-location", {latitude, longitude})
+    }, 
+    (error) => {
+        console.error(error);
+    },
+    {
+        enableHighAccuracy : true,
+        timeout : 5000,
+        maximumAge : 0,
+    });
+}
