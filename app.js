1d477544c414bd03ded8073ea1564b6d71ca1ad9
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-10-04 00:46:21 +0530

    Backend Deployed

--- app.js
+++ app.js
@@ -12,7 +12,13 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public"))); // Corrected line
 
 io.on("connection", function(socket) {
-    console.log("Connected");
+    socket.on("send-location", function(data) {
+        io.emit("receive-location", {id : socket.id, ...data})
+    })
+    
+    socket.on("disconnect", function() {
+        io.emit("user-disconnected", socket.id)
+    })
 });
 
 app.get("/", function(req, res) {
