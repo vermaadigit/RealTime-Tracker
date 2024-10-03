9b9e111bf0290061c7877381c27fdc3ca2c24f96
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-10-04 00:47:42 +0530

    Frontend Map API Added

--- public/js/script.js
+++ public/js/script.js
@@ -14,3 +14,29 @@ if(navigator.geolocation){
         maximumAge : 0,
     });
 }
+
+const map = L.map("map").setView([0,0], 16)
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution : "Aditya Server API"
+}).addTo(map)
+
+const markers = {}
+
+socket.on("receive-location", (data) => {
+    const {id, latitude, longitude} = data
+    map.setView([latitude, longitude])
+    if(markers[id]){
+        markers[id].setLatLng([latitude, longitude])
+    }
+    else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map)
+    }
+})
+
+socket.on("user-disconnected", (id) => {
+    if(markers[id]){
+        map.removeLayer(markers[id])
+        delete markers[id]
+    }
+})
